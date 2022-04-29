8c560c414e13a9c3364379d13274dfe0459abdad
Tuan Nguyen <from.tuan2@gmail.com>
2022-04-29 06:53:33 -0500

    [CLOUD-1649] testing skip list expanded

--- scripts/linters/checkov.sh
+++ scripts/linters/checkov.sh
@@ -11,24 +11,24 @@ IFS=$'\n' charts=($dirs)
 set +o noglob
 
 # array of all checks we want to skip
-skip_checks=(
-    CKV_K8S_23
-    CKV_K8S_35
-    CKV_K8S_14
-    CKV_K8S_20
-    CKV_K8S_28
-    CKV_K8S_15
-    CKV_K8S_40
-    CKV_K8S_31
-    CKV_K8S_22
-    CKV_K8S_37
-    CKV_K8S_38
-    CKV_K8S_43
-    CKV_K8S_21
-    )
+# skip_checks=(
+#     CKV_K8S_23
+#     CKV_K8S_35
+#     CKV_K8S_14
+#     CKV_K8S_20
+#     CKV_K8S_28
+#     CKV_K8S_15
+#     CKV_K8S_40
+#     CKV_K8S_31
+#     CKV_K8S_22
+#     CKV_K8S_37
+#     CKV_K8S_38
+#     CKV_K8S_43
+#     CKV_K8S_21
+#     )
 
 # join the array of checks to skipchecks with a comma
-printf -v skipchecks "%s," "${skip_checks[@]}"
+#printf -v skipchecks "%s," "${skip_checks[@]}"
 
 error_sum=0 # total of all all error codes
 
@@ -37,11 +37,11 @@ for c in "${charts[@]}"; do
     checkov -d "$c" --var-file "$c/ci/default-values.yaml" \
     --framework helm \
     --quiet \
-    --skip-check $skipchecks
+    --skip-check CKV_K8S_23,CKV_K8S_35,CKV_K8S_14,CKV_K8S_20,CKV_K8S_28,CKV_K8S_15,CKV_K8S_40,CKV_K8S_31,CKV_K8S_22,CKV_K8S_37,CKV_K8S_38,CKV_K8S_43,CKV_K8S_21
 
     #track any checkov errors
     ((error_sum=error_sum+$?))
-done;
+done
 
 if [ $error_sum -ne 0 ]; then
     echo "Checkov failed for some charts"
